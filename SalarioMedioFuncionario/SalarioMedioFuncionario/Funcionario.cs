ee5490d4cafc15ed7c8f0bd46170f11d26d5008b
Alef Santos Dias <161088634+AlefSantosDev@users.noreply.github.com>
2026-09-04 18:17:42 -0300

    Firts Commit

---
+++ SalarioMedioFuncionario/SalarioMedioFuncionario/Funcionario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalarioMedioFuncionario
+{
+    class Funcionario
+    {
+        public string nome;
+        public double salario;
+    }
+}
---
+++ SalarioMedioFuncionario/SalarioMedioFuncionario/Program.cs
@@ -0,0 +1,29 @@
+﻿using SalarioMedioFuncionario;
+
+namespace SalarioMediaFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionario");
+            Console.Write("Nome: ");
+            f1.nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            f1.salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do segundo funcionario");
+            Console.Write("Nome: ");
+            f2.nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            f2.salario = double.Parse(Console.ReadLine());
+
+            double media = (f1.salario + f2.salario) / 2;
+
+            Console.WriteLine("salario medio: " + media.ToString("F2"));
+        }
+    }
+}
